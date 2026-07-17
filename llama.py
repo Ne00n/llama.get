80c59d6685afd713ad10ac5b427ec0c3e60a9f5f
Ne00n <neo@tiefkuehler-madness.me>
2026-07-18 00:16:20 +0200

    base

---
+++ llama.py
@@ -0,0 +1,40 @@
+import subprocess, requests, json
+
+print("Fetching models...")
+try:
+    req = requests.get("https://raw.githubusercontent.com/Ne00n/llama.get/refs/heads/master/models.json", timeout=(5,5))
+    if req.status_code != 200: raise Exception(req.status_code)
+    modelList = req.json()
+except Exception as e:
+    exit(f"Unable to fetch/load models.json: {e}")
+
+for nameOrSo, models in modelList.items():
+    print(f"Checking {nameOrSo}")
+    goal = 6
+    for model in models:
+        splitted = model.split("/")
+        print(f"Getting {splitted[1]} from {splitted[0]}")
+        try:
+            req = requests.get(f"https://huggingface.co/api/models/{model}/tree/main", timeout=(5,5))
+            if req.status_code != 200: raise Exception(req.status_code)
+            files = req.json()
+        except Exception as e:
+            print(f"Unable to fetch file list for model {splitted[1]}")
+        availableMemory = 8
+        targets = ["Q6_K.gguf","Q6_K_XL.gguf","Q4_K_XL.gguf","Q4_K_M.gguf"]
+        solutions = {"gguf":"","ggufSize":0,"mmproj":"","mmprojSize":0}
+        for file in files:
+            size = int(file['size'] / 1024**3)
+            if size >= availableMemory: continue
+            for target in targets:
+                if target in file['path']:
+                    if solutions['ggufSize'] < int(file['size']):
+                        solutions["gguf"] = file['path']
+                        solutions['ggufSize'] = int(file['size'])
+                    break
+            if "mmproj" in file['path'] and solutions['mmprojSize'] < int(file['size']):
+                solutions["mmproj"] = file['path']
+                solutions['mmprojSize'] = int(file['size'])
+        if solutions['gguf']:
+            print(f"Fetching {solutions['gguf']}")
+            result = subprocess.getoutput(f'hf download --include "{solutions['gguf']}" --local-dir models/ {model}')
